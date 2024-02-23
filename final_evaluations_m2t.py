d8eee8528706d82d122a2ba26b9fbc38e93a5943
Cralence <mahlerrrr76@gmail.com>
2024-02-23 17:42:36 +0800

    update

--- final_evaluations_m2t.py
+++ final_evaluations_m2t.py
@@ -11,6 +11,7 @@ from utils.utils import *
 from bert_score import score
 from nlgeval import NLGEval
 from os.path import join as pjoin
+import sys
 
 import logging
 import transformers
@@ -90,7 +91,7 @@ def evaluate_bleu_rouge_cider(text_loaders, file):
 
         ref_list = [list(refs) for refs in zip(*text_loader.dataset.all_caption_list)]
         cand_list = text_loader.dataset.generated_texts_list
-        print(f'\n\n{ref_list}\n{cand_list}\n')
+        print(f'\n\n{ref_list}\n{cand_list}\n', file=sys.stderr)
         scores = nlg_eval.compute_metrics(ref_list, cand_list)
         bleu_score_dict[text_loader_name] = np.array([scores['Bleu_1'],scores['Bleu_2'],scores['Bleu_3'],scores['Bleu_4']])
         rouge_score_dict[text_loader_name] = scores['ROUGE_L']
