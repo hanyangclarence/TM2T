132e7e85e2af76859f1627fa04fc252ff02b7e4a
Cralence <mahlerrrr76@gmail.com>
2024-02-24 09:15:31 +0800

    update

--- final_evaluations_m2t.py
+++ final_evaluations_m2t.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from text_loaders.dataset_text_loader import get_dataset_text_loader
 from text_loaders.model_text_loaders import get_text_loader
@@ -91,6 +92,11 @@ def evaluate_bleu_rouge_cider(text_loaders, file):
 
         ref_list = [list(refs) for refs in zip(*text_loader.dataset.all_caption_list)]
         cand_list = text_loader.dataset.generated_texts_list
+
+        with open('gt.json', 'w') as f:
+            json.dump(text_loader.dataset.all_caption_list, f, indent=4)
+        with open('pred.json', 'w') as f:
+            json.dump(text_loader.dataset.generated_texts_list, f ,indent=4)
 
         scores = nlg_eval.compute_metrics(ref_list, cand_list)
         bleu_score_dict[text_loader_name] = np.array([scores['Bleu_1'],scores['Bleu_2'],scores['Bleu_3'],scores['Bleu_4']])
